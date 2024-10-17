89523075d00a9d2e9ed85001953a6a32e25f6f7a
hwk9407 <hwk__@naver.com>
2024-10-17 10:17:36 +0900

    fix(EditResponseDto): 빠져있던 @Getter 어노테이션 수정

--- src/main/java/com/sparta/scheduleapp/schedule/dto/response/EditResponseDto.java
+++ src/main/java/com/sparta/scheduleapp/schedule/dto/response/EditResponseDto.java
@@ -1,7 +1,9 @@
 package com.sparta.scheduleapp.schedule.dto.response;
 
 import com.sparta.scheduleapp.entity.Schedule;
+import lombok.Getter;
 
+@Getter
 public class EditResponseDto extends ResponseDto {
     ScheduleDto schedule;
     public EditResponseDto(String message, Schedule schedule) {
