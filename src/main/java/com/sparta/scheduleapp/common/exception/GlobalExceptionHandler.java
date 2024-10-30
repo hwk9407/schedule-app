cc1caee909a34051079208a42b450ef8ae486579
hwk9407 <hwk__@naver.com>
2024-10-30 09:45:22 +0900

    feat(GlobalExceptionHandler): 잘못된 입력 요청, 유효성 검사 실패 시에 대해서 사용자에게 명확한 피드백 제공

--- src/main/java/com/sparta/scheduleapp/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/scheduleapp/common/exception/GlobalExceptionHandler.java
@@ -1,17 +1,53 @@
 package com.sparta.scheduleapp.common.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.sparta.scheduleapp.common.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+    
+    // 토큰을 발견하지 못해서 발생한 예외처리 (Filter에서 발생한 예외를 이곳에서 잡아줄 수 없다..)
     @ExceptionHandler(TokenNotFoundException.class)
-    public ResponseEntity<?> handleTokenNotFound(TokenNotFoundException ex) {
+    public ResponseEntity<ResponseDto> handleTokenNotFound(TokenNotFoundException ex) {
         ResponseDto responseDto = new ResponseDto(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto); // 400
+    }
+
+    // 유효성 검사에서 발생한 모든 오류를 예외처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        // 첫 번째 오류를 가져옵니다.
+        FieldError firstError = ex.getBindingResult().getFieldErrors().stream().findFirst().orElse(null);
+        String errorMessage;
+
+        if (firstError != null) {
+            // 필드 이름과 기본 메시지를 사용하여 오류 메시지를 생성합니다.
+            errorMessage = String.format("필드 '%s'에 대한 유효성 검사 실패: %s",
+                    firstError.getField(),
+                    firstError.getDefaultMessage());
+        } else {
+            errorMessage = "유효성 검사 실패";
+        }
+
+        ResponseDto responseDto = new ResponseDto(errorMessage);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto); // 400
+    }
+
+    // 잘못된 입력요청 때문에 발생한 예외처리
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDto> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        String errorMessage = ex.getMessage();
+        if (ex.getCause() instanceof InvalidFormatException) {
+            errorMessage = "잘못된 입력값입니다.";
+        }
+        ResponseDto responseDto = new ResponseDto(errorMessage);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto); // 400
     }
 }
